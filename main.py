d044a2ade78d01f1cb7738f937dbde36d71f789d
Artur Kondakov <arthurkondakov@yandex.ru>
2023-02-15 00:02:53 +0100

    Add show_table, split_answer_user, show_answer_user

--- main.py
+++ main.py
@@ -11,17 +11,34 @@ with open ("quastion.json", "w") as file:
             "100": {"quastion": "dog", "answer": "собака", "asked": "False"},
             "200": {"quastion": "shark", "answer": "акула", "asked": "False"},
             "300": {"quastion": "table", "answer": "стол", "asked": "False"},
-
         },
         "Еда": {
             "100": {"quastion": "apple", "answer": "яблоко", "asked": "False"},
             "200": {"quastion": "tomato", "answer": "помидор", "asked": "False"},
             "300": {"quastion": "banana", "answer": "банан", "asked": "False"},
-
         }
     }
     file.write(json.dumps(quastion))
 
-with open ("quastion.json", "r") as file:
-    json.loads(file)
-    print(file.read)
+def show_table():
+    with open ("quastion.json", "r") as file:
+        title = json.loads(file.read())
+        for category in title:
+            print(category, end=" ")
+            for point in title[category]:
+                print(point, end=" ")
+            print("\n")
+def show_answer_user(list):
+    with open ("quastion.json", "r") as file:
+        title = json.loads(file.read())
+        print(title[list[0]][list[1]]["quastion"])
+
+
+def show_split_answer(answer):
+    list = (answer.split(" "))
+    return list
+
+show_table()
+answer = input("Выберите категорию и количество баллов\n")
+show_split_answer(answer)
+show_answer_user(show_split_answer(answer))
