318a8591e9565a4e46f8fb4c25a163c83fd08b80
Artur Kondakov <arthurkondakov@yandex.ru>
2023-02-20 15:26:54 +0100

    Добавил перенос строки после ответа

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ quastion = {
             "100": {"quastion":"plane", "answer":"самолет", "asked":"False"},
             "200": {"quastion":"train", "answer":"поезд", "asked":"False"},
             "300": {"quastion":"boarding", "answer": "посадка", "asked": "False"},
+        },
+        "Животные": {
+            "100": {"quastion": "dog", "answer": "собака", "asked": "False"},
+            "200": {"quastion": "shark", "answer": "акула", "asked": "False"},
+            "300": {"quastion": "table", "answer": "стол", "asked": "False"},
+        },
+        "Еда": {
+            "100": {"quastion": "apple", "answer": "яблоко", "asked": "False"},
+            "200": {"quastion": "tomato", "answer": "помидор", "asked": "False"},
+            "300": {"quastion": "banana", "answer": "банан", "asked": "False"},
         }
-        # "Животные": {
-        #     "100": {"quastion": "dog", "answer": "собака", "asked": "False"},
-        #     "200": {"quastion": "shark", "answer": "акула", "asked": "False"},
-        #     "300": {"quastion": "table", "answer": "стол", "asked": "False"},
-        # },
-        # "Еда": {
-        #     "100": {"quastion": "apple", "answer": "яблоко", "asked": "False"},
-        #     "200": {"quastion": "tomato", "answer": "помидор", "asked": "False"},
-        #     "300": {"quastion": "banana", "answer": "банан", "asked": "False"},
-        # }
     }
 tru = 0
 fal = 0
@@ -29,6 +29,7 @@ def show_table():
                 else:
                     print("   ", end='')
             print("\n")
+
 def show_answer_user(list):
         return (quastion[list[0]][list[1]]["quastion"])
 
@@ -43,6 +44,7 @@ def show_count_quastion():
         for point in quastion[categ]:
             sum_quastion += 1
     return sum_quastion
+
 def show_finish(sum_, tru, fal):
     with open ("coint.json", "w") as file:
         coins = [sum_,tru, fal]
@@ -50,22 +52,23 @@ def show_finish(sum_, tru, fal):
     print(f"Сумма очков {sum_}")
     print(f"Всего верных ответов {tru}")
     print(f"Нерпавильных ответов {fal}")
+
 show_table()
 count = show_count_quastion()
-while  count > 0:
+while count > 0:
     answer = input("Выберите категорию и количество баллов\n")
     show_split_answer(answer)
     word = show_answer_user(show_split_answer(answer))
     answer_user = input(f"В переводе: {word} - означает:\n")
     # show_coins(show_split_answer(answer), answer_user)
     if answer_user == quastion[show_split_answer(answer)[0]][show_split_answer(answer)[1]]["answer"]:
-        print(f"Ответ правильный! Ты получаешь {show_split_answer(answer)[1]} очков")
+        print(f"Ответ правильный! Ты получаешь {show_split_answer(answer)[1]} очков\n")
         quastion[show_split_answer(answer)[0]][show_split_answer(answer)[1]]["asked"] = "True"
         sum_ += int(show_split_answer(answer)[1])
         tru += 1
     else:
         quastion[show_split_answer(answer)[0]][show_split_answer(answer)[1]]["asked"] = "True"
-        print(f"Ответ не верный, правильный ответ {quastion[show_split_answer(answer)[0]][show_split_answer(answer)[1]]['answer']}")
+        print(f"Ответ не верный, правильный ответ {quastion[show_split_answer(answer)[0]][show_split_answer(answer)[1]]['answer']}\n")
         fal += 1
     show_table()
     count -= 1
